d8ab9ffa2924db8d6501a7097dd15f00a1625a08
aaryan amol pradhan <grdcfcponly@gmail.com>
2026-09-20 09:03:06 +0530

    added content

--- arrays/Permutation_Order.cpp
+++ arrays/Permutation_Order.cpp
@@ -1,1 +1,73 @@
-love
+// orz
+#include <bits/stdc++.h>
+#include "algodebug.h"
+#define ll long long 
+#define endl "\n"
+#define fr first
+#define sc second
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define mp make_pair
+#define PI acos(-1) 
+#define py cout << "YES" << endl
+#define pn cout << "NO" << endl
+#define pll pair<ll,ll>
+
+using namespace std;
+/*Let p(n,k) denote the kth permutation (in lexicographical order) of 1 \dots n. For example, p(4,1)=[1,2,3,4] and p(4,2)=[1,2,4,3].
+Your task is to process two types of tests:
+
+Given n and k, find p(n,k)
+Given n and p(n,k), find k
+
+Input
+The first line has an integer t: the number of tests.
+Each test is either "1 n k" or "2 n p(n,k)".
+Output
+For each test, print the answer according to the example.
+Constraints
+
+1 \le t \le 1000
+1 \le n \le 20
+1 \le k \le n!
+
+Example
+Input:
+6
+1 4 1
+1 4 2
+2 4 1 2 3 4
+2 4 1 2 4 3
+1 5 42
+2 5 2 4 5 3 1
+
+Output:
+1 2 3 4
+1 2 4 3
+1
+2
+2 4 5 3 1
+42*/
+void solve() 
+{
+    
+   
+
+
+
+
+
+
+}
+signed main() 
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+ 
+    solve() ; 
+}
+    
+  
